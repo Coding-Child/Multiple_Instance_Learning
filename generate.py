6ff008f2e9d6691d55171036c33faa19cb390f80
Suhyeong <pshpulip40@gmail.com>
2024-02-07 19:47:10 +0900

    Update generate.py

--- generate.py
+++ generate.py
@@ -10,7 +10,6 @@ paths.sort(key=lambda x: int(x.split('_')[-1].split('.')[0]))
 
 model = ResNet50('simclr_pretrained_model_ckpt/checkpoint_0200_Adam.pt').cuda()
 
-
 with tqdm(total=len(paths)) as pbar:
     for path in paths:
         generate_patches_json(path, 224, save_path='cancer_data', model=model, n_clusters=2, static=True)
